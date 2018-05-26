babd4f9952ab1aff12ff00f0addde90c3931b0b5
myrobo <blogsk.myrobo@gmail.com>
2018-05-27 01:34:33 +0900

    change_gitpush_cmd

--- scripts/GitPush.sh
+++ scripts/GitPush.sh
@@ -4,6 +4,10 @@ target=".git"
 dir="build"
 max_depth=10
 
+if [ $# -ne 1 ]; then
+	echo "[$0] Args error."
+fi
+
 found="false"
 for i in $(seq 1 ${max_depth})
 do
@@ -17,6 +21,9 @@ done
 
 if [ ${found} = "true" ]; then
 	echo "[$0] Found ${target}."
+	git add --all
+	git commit -m $1
+	git push origin master
 else
 	echo "[$0] Cannot find ${target}."
 fi
